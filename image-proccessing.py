14df2f7123838493b007dfe53c49073c6c607b2b
Haidyn <haidyn1988@gmail.com>
2018-01-27 14:25:20 +1000

    feat: adjust image gamma

--- image-proccessing.py
+++ image-proccessing.py
@@ -3,6 +3,21 @@ import glob
 import numpy as np
 
 
+label = []  # stores the label of each image
+image_data = []  # store the image data
+
+
+football_filenames = [img for img in glob.glob("images/football/*.jpg")]
+tennis_ball_filenames = [img for img in glob.glob("images/tennisball/*.jpg")]
+football_pitch_filenames = [img for img in glob.glob("images/footballpitch/*.jpg")]
+tennis_court_filenames = [img for img in glob.glob("images/tenniscourt/*.jpg")]
+
+football_filenames.sort()
+tennis_ball_filenames.sort()
+football_pitch_filenames.sort()
+tennis_court_filenames.sort()
+
+
 def translate_image(img, label, distance=0, step=20):
     # Translation of image data to create more training data
     # img : image data
@@ -70,132 +85,83 @@ def zoom_image(img, label, ratio=2, steps=2):
         col_offset += col_step
 
         # make the image larger
-        larger_img = img[row_offset:img_size - row_offset, col_offset:img_size - col_offset]
+        larger_img = img[row_offset:rows - row_offset, col_offset:cols - col_offset]
         img_list.append(cv2.resize(larger_img, (rows, cols), interpolation=cv2.INTER_CUBIC))
 
         label_list.append(label)
 
     return img_list, label_list
 
+## Original function sourced from https://www.pyimagesearch.com/2015/10/05/opencv-gamma-correction/
+def adjust_gamma(image, gamma=1.0):
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+                      for i in np.arange(0, 256)]).astype("uint8")
 
-football_filenames = [img for img in glob.glob("images/football/*.jpg")]
-tennis_ball_filenames = [img for img in glob.glob("images/tennisball/*.jpg")]
-football_pitch_filenames = [img for img in glob.glob("images/footballpitch/*.jpg")]
-tennis_court_filenames = [img for img in glob.glob("images/tenniscourt/*.jpg")]
-
-football_filenames.sort()
-tennis_ball_filenames.sort()
-football_pitch_filenames.sort()
-tennis_court_filenames.sort()
-
-label = []  # stores the label of each image
-image_data = []  # store the image data
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
 
 
-shif_dist = 0
-zoom_ratio = 1.5
-img_size = 256
-
-for name in football_filenames:
-    img = cv2.imread(name)
-    name = "football"
-
-    # resize to the GoogLenet input size of 256x256x3
-    img = cv2.resize(img, (img_size, img_size), interpolation=cv2.INTER_CUBIC)
+def more_images(img, label_name):
+    shif_dist = 0
+    zoom_ratio = 1.5
 
     # translate base image
-    img_list, label_list = translate_image(img, name, step=shif_dist//2,
+    img_list, label_list = translate_image(img, label_name, step=shif_dist // 2,
                                            distance=shif_dist)
     image_data.extend(img_list)
     label.extend(label_list)
 
-    img_list, label_list = zoom_image(img, name, ratio=zoom_ratio, steps=2)
-    image_data.extend(img_list)
-    label.extend(label_list)
-
-
-    # translate horizontally flipped image
-    flip_img = cv2.flip(img, 1)
-    img_list, label_list = translate_image(flip_img, name, step=shif_dist // 2,
-                                           distance=shif_dist)
     # scale the flipped image
-    zoom_img_list, zoom_label_list = zoom_image(flip_img, name, ratio=zoom_ratio, steps=2)
-
+    img_list, label_list = zoom_image(img, label_name, ratio=zoom_ratio, steps=2)
     image_data.extend(img_list)
     label.extend(label_list)
-    image_data.extend(zoom_img_list)
-    label.extend(zoom_label_list)
+
+def make_data(filenames, label_name, img_size=256):
+
+    for name in filenames:
+        image = cv2.imread(name)
+
+        if image == None:  # skip corrupt image data
+            continue
+
+        # adjust image gamma for more data
+        for i in range(0, 3):
+
+            if i <= 1: # 0.5 and 1.0
+                gamma = 0.5 + i * 0.5
+            else:
+                gamma = 1 + (i - 1) * 1. # 2.0
+
+            img = adjust_gamma(image, gamma)
+
+            # resize to the GoogLenet input size of 256x256x3
+            img = cv2.resize(img, (img_size, img_size), interpolation=cv2.INTER_CUBIC)
+
+            # translate base image
+            more_images(img, label_name)
+
+            # translate horizontally flipped image
+            flip_img = cv2.flip(img, 1)
+            more_images(flip_img, label_name)
+
+            # translate vertically flipped image
+            flip_img = cv2.flip(img, 0)
+            more_images(flip_img, label_name)
+
+            # translate vertically and horizontally flipped image
+            flip_img = cv2.flip(img, -1)
+            more_images(flip_img, label_name)
 
 
-    # translate vertically flipped image
-    flip_img = cv2.flip(img, 0)
-    img_list, label_list = translate_image(flip_img, name, step=shif_dist // 2,
-                                           distance=shif_dist)
-    # scale the flipped image
-    zoom_img_list, zoom_label_list = zoom_image(flip_img, name, ratio=zoom_ratio, steps=2)
+if __name__ == "__main__":
+    make_data(football_filenames, "football")
+    make_data(tennis_ball_filenames, "tennis_ball")
+    make_data(football_pitch_filenames, "nothing")
+    make_data(tennis_court_filenames, "nothing")
 
-    image_data.extend(img_list)
-    label.extend(label_list)
-    image_data.extend(zoom_img_list)
-    label.extend(zoom_label_list)
+    print(len(image_data))
 
 
-    # translate vertically and horizontally flipped image
-    flip_img = cv2.flip(img, -1)
-    img_list, label_list = translate_image(flip_img, name, step=shif_dist // 2,
-                                           distance=shif_dist)
-    # scale the flipped image
-    zoom_img_list, zoom_label_list = zoom_image(flip_img, name, ratio=zoom_ratio, steps=2)
-
-    image_data.extend(img_list)
-    label.extend(label_list)
-    image_data.extend(zoom_img_list)
-    label.extend(zoom_label_list)
-
-
-print(len(image_data))
-
-
-
-
-
-
-'''# Create normalised images for the training data
-print('Creating the data')
-with open('./data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        angle = float(line[3])
-
-        # only keep data with a steer angle
-        if angle >= 0.01 or angle <= -0.01:
-            for camera in range(0, 3):
-                name = './data/IMG/' + line[camera].split('/')[-1]
-                image = cv2.imread(name)
-
-                # Left of image is a negative angle, Right positive
-                if camera == 1:
-                    # Left camera, make angle smaller to help return to center
-                    angle += 0.3
-                    distance = 0
-                elif camera == 2:
-                    # Right camera, make angle smaller to help return to center
-                    angle -= 0.3
-                    distance = 0
-                else:
-                    distance = 0
-
-                img_list, angle_list = translate_image(image, angle,  step=20,
-                                                       distance=distance)
-                drive_data.extend(img_list)
-                steer_data.extend(angle_list)
-
-                img_list, angle_list = translate_image(np.fliplr(image), -angle,
-                                                       distance=distance)
-                drive_data.extend(img_list)
-                steer_data.extend(angle_list)
-
-print('data count: ', np.shape(drive_data))
-train_data, val_data, train_labels, val_labels = train_test_split(drive_data,
-                                                                  steer_data,
-                                                                  test_size=0.15)'''
